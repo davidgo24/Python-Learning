39833abc5b6866f58150a5f3f8a1b734bc239e77
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-18 17:07:35 -0800

    update loops.py
    completed the loops section

--- course1-learningpython/loops.py
+++ course1-learningpython/loops.py
@@ -48,3 +48,72 @@ for i in range(0, 10, 2):
 # 4
 # 6
 # 8
+
+'''Range Continued'''
+def count_down(start, end):
+    for i in range(start, end, -1):
+        print(i)
+        #counts from start to end arguments by -1
+'''F-Strings in Python'''
+#you can create a string with dynamic values by using f-strings
+num_bananas = 10
+print(f"You have {num_bananas} bananas")
+
+'''#2'''
+def check_for_meals_and_retirement(starting_age, ending_age):
+    for age in range(starting_age, ending_age):
+        if age < 8:
+            print(f"You qualify for free meals. You are {age} years old.")
+        elif age > 65:
+            print(f"You qualify for retirement. You are {age} years old.")
+    
+'''increment and decrement'''
+
+number_of_enemies = 10
+number_of_enemies += 2
+# number_of_enemies is 12
+number_of_enemies = 10
+number_of_enemies -= 2
+# number_of_enemies is 8
+'''#3'''
+def sum_of_numbers(start, end):
+    total = 0
+    for i in range(start, end):
+        total += i
+    return total
+'''#4'''
+def sum_of_odd_numbers(end):
+    total = 0
+    for i in range(1, end, 2):
+        total += i
+    return total
+'''Optional Challenges'''
+#Rocket Launch
+def countdown_to_blastoff():
+    for num in range(10, 0, -1):
+        if num > 1:
+            print(f"{num}. . .")
+        elif num == 1:
+            print(f"{num}. . . Blastoff!")
+
+#SQUARING Numbers
+def calculate_squares(start, end):
+
+    for num in range(start, end):
+        num_squared = num ** 2 ## using the ** operator
+        print(f"{num} squared = {num_squared}")
+#the importance of having a counter and using increment (pretend you are adding to a scoreboard)
+def calculate_experience_points(level):
+    xp = 0 
+    for num in range(level):
+        xp += num * 5
+    return xp
+
+#DISCOVER PRIME NUMBER TOOL
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
