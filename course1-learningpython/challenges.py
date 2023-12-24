4100b48207205572333a73948b089c18f519a675
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-24 13:48:41 -0800

    update challenges.py

---
+++ course1-learningpython/challenges.py
@@ -0,0 +1,26 @@
+'''Challenge #1: Number sum '''
+
+#NUMBER SUM
+#Write a function called number_sum(n) that adds up all the numbers from 1 to n. For example:
+
+#number_sum(5) -> 1+2+3+4+5 -> 15
+
+#number_sum(3) -> 1+2+3 -> 6
+
+#my solution
+def number_sum(n):
+    sum_list = []
+    for i in range(0, n + 1):
+        sum_list.append(i)
+    return sum(sum_list)
+        
+#alternative method
+ 
+def number_sum(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    return sum
+
+
+'''Challenge #2: Minimum Numbers in Python'''
