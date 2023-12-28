cbd0d8e303bdd43be754d52daa70000041cc7523
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-28 14:08:08 -0800

    inheritanceoop.py

--- course1-learningpython/inheritanceoop.py
+++ course1-learningpython/inheritanceoop.py
@@ -103,7 +103,7 @@ class Crossbowman(Archer):
     def take_damage(self, damage):
         self.__health -= damage
 
-'''Inheritance with Multiple Children'''
+'''Inheritance with Multiple Children Part 1'''
 
 class Archer(Hero):
     def __init__(self, name, health, num_arrows):
@@ -118,3 +118,45 @@ class Archer(Hero):
         self.__num_arrows -= 1
         target_damage = target.take_damage(10)
         
+'''Inheritance with multiple children Part 2'''
+class Hero:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = health
+
+    def get_name(self):
+        return self.__name
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        self.__health -= damage
+
+
+class Archer(Hero):
+    def __init__(self, name, health, num_arrows):
+        super().__init__(name, health)
+        self.name = name
+        self.health = health
+        self.__num_arrows = num_arrows
+
+    def shoot(self, target):
+        if self.__num_arrows == 0:
+            raise Exception("not enough arrows")
+        self.__num_arrows -= 1
+        target_damage = target.take_damage(10)
+
+
+class Wizard(Hero):
+    def __init__(self, name, health, mana):
+        super().__init__(name, health)
+        self.name = name
+        self.health = health
+        self.__mana = mana
+
+    def cast(self, target):
+        if self.__mana < 25:
+            raise Exception("not enough mana")
+        self.__mana -= 25
+        target.take_damage(25)
