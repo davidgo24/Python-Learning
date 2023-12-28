ac7fd9fb4a88141aa8c3166fd33e1c89c7769852
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-28 08:33:42 -0800

    update abstractionoop.py

--- course1-learningpython/abstractionoop.py
+++ course1-learningpython/abstractionoop.py
@@ -39,3 +39,90 @@ class Human:
         return tuple_position
 
 
+#i am starting to get the bigger picture
+
+''''spring abstraction assignment'''
+class Human:
+    def sprint_right(self):
+        self.__raise_if_cannot_sprint()
+        self.__use_sprint_stamina()
+        self.move_right()
+        self.move_right()
+
+    def sprint_left(self):
+        self.__raise_if_cannot_sprint()
+        self.__use_sprint_stamina()
+        self.move_left()
+        self.move_left()
+
+
+    def sprint_up(self):
+        self.__raise_if_cannot_sprint()
+        self.__use_sprint_stamina()
+        self.move_up()
+        self.move_up()
+
+    def sprint_down(self):
+        self.__raise_if_cannot_sprint()
+        self.__use_sprint_stamina()
+        self.move_down()
+        self.move_down()
+
+
+    def __raise_if_cannot_sprint(self):
+        if self.__stamina == 0:
+            raise Exception("not enough stamina to sprint")
+
+    def __use_sprint_stamina(self):
+        self.__stamina -= 1
+
+    # don't touch below this line
+
+    def move_right(self):
+        self.__pos_x += self.__speed
+
+    def move_left(self):
+        self.__pos_x -= self.__speed
+
+    def move_up(self):
+        self.__pos_y += self.__speed
+
+    def move_down(self):
+        self.__pos_y -= self.__speed
+
+    def get_position(self):
+        return self.__pos_x, self.__pos_y
+
+    def __init__(self, pos_x, pos_y, speed, stamina):
+        self.__pos_x = pos_x
+        self.__pos_y = pos_y
+        self.__speed = speed
+        self.__stamina = stamina
+
+
+'''HOW OOP DEVELOPERS THINK'''
+
+#As we saw in the last exercise variables can be private, 
+# but methods can be private as well. In other words, we can encapsulate behavior as well as data.
+
+'''GROUPING DATA AND BEHAVIOR'''
+#Classes in object-oriented programming are all about grouping data and behavior together in one place: an object.  *
+# Object-oriented programmers tend to think about programming as a modeling problem. 
+# They think, "how can I write a Human class that simulates the data and behavior of a real human?" **
+#think of oop as building a controller with a class. this controller has a ton of buttons (methods) 
+
+'''functional programming vs oop'''
+#We aren't focusing on functional programming in this course, but to provide some contrast,
+# functional programmers tend to think of their code as inputs and outputs. "When a human acts, what are the inputs to that action, and how do the outputs affect my program state?"
+
+#While OOP isn't the only paradigm in programming, it's a tried and true one that is useful in a variety of circumstances.
+# In any event, if you have an understanding of multiple ways of thinking about code, you'll be a much better developer overall.
+
+
+#oop devs think of programs as a modeling problem
+
+
+
+
+
+
