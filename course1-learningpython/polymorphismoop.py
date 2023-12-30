ee0a933d036199195cd6f949f88cc94d39ef6367
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-29 17:20:34 -0800

    update polymorphismoop.py

--- course1-learningpython/polymorphismoop.py
+++ course1-learningpython/polymorphismoop.py
@@ -22,3 +22,19 @@ for creature in [Creature(), Dragon(), Kraken()]:
 # the creature moves
 # the dragon flies
 # the kraken swims
+
+
+'''example #1'''
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2 
+        self.y2 = y2 
+        print(f"corner1: ({x1},{y1}) corner2: ({x2},{y2})")
+        
+        
+        
+        
+        
+        
