6cda4f4c460f2872ac772efc004786457b4e4e42
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2023-12-31 14:14:06 -0800

    update functionalprogramming.py

--- course1-learningpython/functionalprogramming.py
+++ course1-learningpython/functionalprogramming.py
@@ -15,4 +15,24 @@ return num
 #functional -The important distinction is that in the functional example, we don't need to mutate (change) the values of any variables. There is no statefulness.
 return transform_b(transform_a(get_a()))
 
+'''assignment #1'''
+
+def stylize_title(document):
+    pass
+
+
+# Don't touch below this line
+
+
+def center_title(document):
+    width = 40
+    title = document.split("\n")[0]
+    centered_title = title.center(width)
+    return document.replace(title, centered_title)
+
+
+def add_border(document):
+    title = document.split("\n")[0]
+    border = "*" * len(title)
+    return document.replace(title, title + "\n" + border)
 
