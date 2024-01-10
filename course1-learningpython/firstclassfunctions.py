c98f1486f2fe5754c721d981ad276b48bbfbb6aa
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2024-01-10 14:45:21 -0800

    update firstclassfunctions.py

--- course1-learningpython/firstclassfunctions.py
+++ course1-learningpython/firstclassfunctions.py
@@ -51,3 +51,15 @@ def square(x):
 '''higher-order function'''
 #a function that accepts another function as an arg or returns a function
 
+def square(x):
+    return x * x
+
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+squares = my_map(square, [1, 2, 3, 4, 5])
+print(squares)
+# [1, 4, 9, 16, 25]
