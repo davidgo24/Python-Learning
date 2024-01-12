d0521b7e8217be941e5d70b5f0e207ee5861912e
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2024-01-11 19:52:35 -0800

    update firstclassfunctions.py

--- course1-learningpython/firstclassfunctions.py
+++ course1-learningpython/firstclassfunctions.py
@@ -63,3 +63,16 @@ def my_map(func, arg_list):
 squares = my_map(square, [1, 2, 3, 4, 5])
 print(squares)
 # [1, 4, 9, 16, 25]
+
+
+def change_bullet_style(document):
+    return "\n".join(map(convert_line, document.split('\n')))
+# Don't edit below this line
+
+
+def convert_line(line):
+    old_bullet = "-"
+    new_bullet = "*"
+    if len(line) > 0 and line[0] == old_bullet:
+        return new_bullet + line[1:]
+    return line
